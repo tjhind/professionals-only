bd6bc4c269577e6a1c8c5be9c6216ee335879505
tjhind <tjhind9@sky.com>
2024-03-20 21:14:09 +0000

    update header alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <section className="flex justify-center pb-6 bg-white">
+      <section className="flex justify-left pb-6 bg-white">
         <img
           className="h-9 w-8 mr-2 pb-1 mt-2 md:h-10 md:w-10 md:mt-5"
           src={"https://cdn-icons-png.flaticon.com/128/639/639394.png"}
