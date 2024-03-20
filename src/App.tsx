dd4f1f4e432e9bd6b99f189c380c67076b7bdd56
tjhind <tjhind9@sky.com>
2024-03-20 15:42:35 +0000

    set up components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
+import UserForm from "./components/ProfileForm";
+
 export default function App() {
   return (
-    <main className="flex flex-col">
-      <header className="flex h-24 bg-light-green">
-        <img src="/logo.png" height={64} className="h-16 m-auto" />
-      </header>
-    </main>
+    <>
+      <Header />
+      <UserForm />
+    </>
   );
 }
