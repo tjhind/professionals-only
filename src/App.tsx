f2c9b0fa240a3d02f25a5c402b87a8603cf57ebc
tjhind <tjhind9@sky.com>
2024-03-21 14:13:47 +0000

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ProfileForm from "./components/ProfileForm";
+import ProfileCard from "./components/ProfileCard";
 
 export default function App() {
   return (
     <div className="mx-2 md:mx-10">
       <Routes>
-        <Route path="ProfileForm" element={<ProfileForm />} />
+        <Route path="profile-card" element={<ProfileCard />} />
       </Routes>
       <Header />
       <ProfileForm />
