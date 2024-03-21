321041f5a3f63bc0ba255771cec5d7e38378bf45
tjhind <tjhind9@sky.com>
2024-03-21 14:07:39 +0000

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import UserForm from "./components/ProfileForm";
+import ProfileForm from "./components/ProfileForm";
 
 export default function App() {
   return (
     <div className="mx-2 md:mx-10">
+      <Routes>
+        <Route path="ProfileForm" element={<ProfileForm />} />
+      </Routes>
       <Header />
-      <UserForm />
+      <ProfileForm />
     </div>
   );
 }
